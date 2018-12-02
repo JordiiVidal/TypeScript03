b53a1011d870407ca429fd174edf38ec3bd0e504
Jordi Vidal <jordividal1997@gmail.com>
2018-12-02 02:22:59 +0100

    ui

--- extra.js
+++ extra.js
@@ -1,13 +1,43 @@
 var Extra = /** @class */ (function () {
-    function Extra(precio, url) {
+    function Extra(precio, nombre, url) {
         this.precio = precio;
+        this.nombre = nombre;
         this.url = url;
     }
     Extra.prototype.getHtml = function () {
-        return "<div class='card' style='width: 18rem;'><img class='card-img-top' src='" + this.url + "' alt='Card image cap'><div class='card-body'><h5 class='card-title'>Precio</h5><p class='card-text'>" + this.precio + "</p></div></div>";
+        return "<div class='col-2'><div class='card' style='width:100%;'><img class='card-img-top' src='" + this.url + "' alt='Card image cap'><div class='card-body'><h5 class='card-title'>Extra : " + this.nombre + "</h5><p class='card-text'>Precio : " + this.precio + "</p></div></div></div>";
     };
     return Extra;
 }());
-var extra = new Extra(12, 'assets/gucci.jpg');
-var container = document.getElementById("imagen");
+var Coche = /** @class */ (function () {
+    function Coche(nombre, url, velocidad) {
+        this.nombre = nombre;
+        this.velocidad = velocidad;
+        this.url = url;
+        this.extras = [];
+    }
+    Coche.prototype.addExtra = function (extra) {
+        this.extras.push(extra);
+    };
+    Coche.prototype.getHtml = function () {
+        var html = "<div class='card col-4' style='width: 18rem;'><img class='card-img-top' src='" + extra.url + "' alt='Card image cap'><div class='card-body'><h5 class='card-title'>" + this.nombre + "</h5>" + this.velocidad + "</div></div>";
+        html += "<div clas='col-8' style='padding:20px;'><h3>Extas</h3><div class='card-group'>";
+        for (var _i = 0, _a = this.extras; _i < _a.length; _i++) {
+            var extra_1 = _a[_i];
+            html += "<div class='card' style='width: 18rem;margin:10px;'><img class='card-img-top' src='" + extra_1.url + "' alt='Card image cap'><div class='card-body'><h5 class='card-title'>Extra : " + extra_1.nombre + "</h5><p class='card-text'>Precio : " + extra_1.precio + "</p></div></div>";
+        }
+        html += "</div>";
+        return html;
+    };
+    return Coche;
+}());
+var extra = new Extra(12, 'Bolso Gucci', 'assets/gucci.jpg');
+var extra2 = new Extra(12, 'Cinturon Gucci', 'assets/gucci.jpg');
+var container = document.getElementById("extras");
+var container2 = document.getElementById("coches");
 container.innerHTML = extra.getHtml();
+container.innerHTML += extra2.getHtml();
+var coche = new Coche("Golf IV", '', 200);
+coche.addExtra(extra);
+coche.addExtra(extra2);
+container2.innerHTML = coche.getHtml();
--- extra.ts
+++ extra.ts
@@ -1,33 +1,60 @@
+
 class Extra{
 
     precio: number;
+    nombre: string;
     url: string;
 
-    constructor(precio : number,url: string){
+    constructor(precio : number, nombre:string ,url: string){
         this.precio = precio;
+        this.nombre = nombre;
         this.url = url;
     }
 
     getHtml(){
-        return "<div class='card' style='width: 18rem;'><img class='card-img-top' src='"+this.url+"' alt='Card image cap'><div class='card-body'><h5 class='card-title'>Precio</h5><p class='card-text'>"+this.precio+"</p></div></div>";
+        return "<div class='col-2'><div class='card' style='width:100%;'><img class='card-img-top' src='"+this.url+"' alt='Card image cap'><div class='card-body'><h5 class='card-title'>Extra : "+this.nombre+"</h5><p class='card-text'>Precio : "+this.precio+"</p></div></div></div>";
     }
 }
+
 class Coche{
 
     nombre: string;
+    url: string;
     velocidad: number;
-    extras: Extra[];
+    extras: Array<Extra>;
 
-    constructor(nombre:string, velocidad:number, extras: Extra[]){
+    constructor(nombre:string, url:string,velocidad:number){
         this.nombre = nombre;
         this.velocidad = velocidad;
-        this.extras = extras;
+        this.url = url;
+        this.extras = [];
     }
     addExtra(extra:Extra){
         this.extras.push(extra);
     }
+    getHtml(){
+        var html = "<div class='card col-4' style='width: 18rem;'><img class='card-img-top' src='"+extra.url+"' alt='Card image cap'><div class='card-body'><h5 class='card-title'>"+this.nombre+"</h5>"+this.velocidad+"</div></div>";
+        html += "<div clas='col-8' style='padding:20px;'><h3>Extas</h3><div class='card-group'>";
+        for (let extra of this.extras) {
+            html +="<div class='card' style='width: 18rem;margin:10px;'><img class='card-img-top' src='"+extra.url+"' alt='Card image cap'><div class='card-body'><h5 class='card-title'>Extra : "+extra.nombre+"</h5><p class='card-text'>Precio : "+extra.precio+"</p></div></div>";
+
+        }
+        html += "</div>";
+        return html;
+
+    }
 
 }
-let extra = new Extra(12,'assets/gucci.jpg');
-let container = <HTMLElement>document.getElementById("imagen");
+
+let extra = new Extra(12,'Bolso Gucci','assets/gucci.jpg');
+let extra2 = new Extra(12,'Cinturon Gucci','assets/gucci.jpg');
+
+let container = <HTMLElement>document.getElementById("extras");
+let container2 = <HTMLElement>document.getElementById("coches");
+
 container.innerHTML = extra.getHtml();
+container.innerHTML += extra2.getHtml();
+let coche = new Coche("Golf IV",'', 200);
+coche.addExtra(extra);
+coche.addExtra(extra2);
+container2.innerHTML = coche.getHtml();
